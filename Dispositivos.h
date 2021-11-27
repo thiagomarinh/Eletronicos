dd09ec83851cb22eb6200380ab15027fd02b9b9d
thiagomarinh <thiagoo_100@hotmail.com>
2021-11-27 15:21:25 -0300

    criação da classe Dispositivos e do Objeto Computadores
    Na classe Dispositivos foi criado um metodo chamado imprimeMensagem, e foi chamado pelo objeto computadores

---
+++ Dispositivos.h
@@ -0,0 +1,16 @@
+#ifndef DISPOSITIVOS_H
+#define DISPOSITIVOS_H
+
+#include <iostream>
+using std::cout;
+
+class Dispositivos
+{
+public:
+    void imprimeMensagem() const
+    {
+        cout << "Bem vindo a este dispositivo!";
+    }
+
+};
+#endif // GRADEBOOK_H
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using std::cout;
+
+#include "Dispositivos.h"
+
+int main()
+{
+    Dispositivos computadores;
+    computadores.imprimeMensagem();
+    return 0;
+}
